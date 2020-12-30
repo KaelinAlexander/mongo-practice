fc2ab442b08e0dd17f19b0dd778bf2b97a0e53a6
KaelinAlexander <kaelin.alexander@gmail.com>
2020-12-30 11:39:52 -0700

    adds sample test model

---
+++ test/demo_test.js
@@ -0,0 +1,10 @@
+const mocha = require('mocha');
+const assert = require('assert');
+
+//Describe Tests
+describe('some demo tests', function() {
+    // Create Tests
+    it('adds two numbers together', function(){
+        assert(2 + 3 === 5)
+    })
+});
