3a7c891176f5a307a8ee6926d8944a4c4dcb7583
KaelinAlexander <kaelin.alexander@gmail.com>
2020-12-31 15:41:35 -0700

    adds es6 promise interface in lieu of mongoose default; adds hook to establis connection before tests run

--- test/connection.js
+++ test/connection.js
@@ -1,11 +1,18 @@
 const mongoose = require('mongoose')
 
-// Connect to mongodb
+// ES6 Promises
 
-mongoose.connect('mongodb://localhost/testaroo', { useUnifiedTopology: true, useNewUrlParser: true } );
+mongoose.Promise = global.Promise
 
-mongoose.connection.once('open', function() {
-    console.log('Connection has been made successfully. Hooray!')
-}).on('error', function(error){
-    console.log('Connection error:', error)
+// Connect to DB before tests run:
+
+before(function(done){
+    // Connect to mongodb
+    mongoose.connect('mongodb://localhost/testaroo', { useUnifiedTopology: true, useNewUrlParser: true } );
+    mongoose.connection.once('open', function() {
+        console.log('Connection has been made successfully. Hooray!');
+        done();
+    }).on('error', function(error){
+        console.log('Connection error:', error)
+    })  
 })
