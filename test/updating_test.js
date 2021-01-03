371121c5aa1526c91723aa03bfabf99e1fe49eae
KaelinAlexander <kaelin.alexander@gmail.com>
2021-01-03 12:10:21 -0700

    models increment update operator

--- test/updating_test.js
+++ test/updating_test.js
@@ -19,9 +19,18 @@ describe('Updating records', function() {
     });
 
     it('Updates one record from the database', function(done){
-        PokemonChar.findOneAndUpdate({name: 'Clefairy'}, {name: 'Clefable', dexnum: 36}).then(function(){
+        PokemonChar.findOneAndUpdate({name: 'Clefairy'}, {name: 'Clefable'}).then(function(){
             PokemonChar.findOne({_id: char._id}).then(function(result){
                 assert(result.name === 'Clefable');
+                done();
+            })
+        })
+    });
+
+    it('Increments dexnum by one', function(done){
+        PokemonChar.update({}, { $inc: { dexnum: 1 } }).then(function() {
+            PokemonChar.findOne({name: 'Clefairy'}).then(function(record){
+                assert(record.dexnum === 36);
                 done();
             })
         })
